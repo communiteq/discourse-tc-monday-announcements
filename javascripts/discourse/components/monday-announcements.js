acdfd2e9a738cdc17307b6779589c3b3c4e6a60f
Richard <richard@communiteq.com>
2023-05-19 08:44:09 +0000

    Replace tag+excerpts with settings

--- javascripts/discourse/components/monday-announcements.js
+++ javascripts/discourse/components/monday-announcements.js
@@ -13,7 +13,9 @@ export default class MondayAnnoucements extends Component {
             this.router = api.container.lookup('service:router');
             api.onPageChange((url, title) => {
                 var routeInfo = this.router.recognize(url);
-                if ((routeInfo.name == 'tags.showCategory') || (routeInfo.name == 'tags.showCategoryNone') || (routeInfo.name == 'discovery.category')) {
+                console.log(routeInfo);
+                if ((routeInfo.name == 'tags.showCategory') || (routeInfo.name == 'tags.showCategoryNone')
+                    || (routeInfo.name == 'discovery.category') || (routeInfo.name == 'discovery.categoryNone')) {
                     var param = routeInfo.params.category_slug_path_with_id || '';
                     if (param.startsWith(settings.announcements_category_slug)) {
                         this.mustShow = true;
@@ -26,28 +28,8 @@ export default class MondayAnnoucements extends Component {
                 }
 
                 if (this.mustShow) {
-                    ajax(`/tag/${settings.announcements_tag_name}/l/latest.json`).then((result) => {
-                        var announcement_topics = result.topic_list.topics.slice(0,3);
-                        var topic_ids = announcement_topics.map((topic) => topic.id);
-                        var tooltipURL = '/tooltip-previews?fetch';
-                        topic_ids.forEach((id) => {
-                            tooltipURL = tooltipURL + '&topic_ids[]=' + id;
-                        });
-                        ajax(tooltipURL).then((ttResult) => {
-                            topic_ids.forEach((id) => {
-                                // Find the corresponding topic in the topicList by ID
-                                const topic = announcement_topics.find((topic) => topic.id === id);
-
-                                // Insert the newField into the topic
-                                if (topic && ttResult.excerpts[id]) {
-                                    var ex = ttResult.excerpts[id].excerpt;
-                                    var nli = ex.indexOf("\n");
-                                    topic.excerpt = (nli !== -1) ? ex.substring(0, nli) : ex;
-                                }
-                            });
-                            this.announcement_topics = announcement_topics;
-                        });
-                    });
+                    console.log(settings.announcements_topics);
+                    this.announcement_topics = JSON.parse(settings.announcements_topics);
                 }
             });
         });
